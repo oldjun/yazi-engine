d6e919a087a69f5245d910909dca3f7dfd76b583
unknown <oldjun@sina.com>
2020-03-05 16:25:08 +0800

    a c++ work flow based on plugin

--- main.cpp
+++ main.cpp
@@ -1,56 +1,23 @@
 #include <iostream>
 #include <string>
-#include <stdexcept>
 #include <stdio.h>
-#include <sys/resource.h>
-#include <time.h>
-#include <errno.h>
 using namespace std;
 
-#include "Logger.h"
-#include "IniFile.h"
+#include "System.h"
 #include "Singleton.h"
 using namespace yazi::utility;
 
 #include "Workflow.h"
 using namespace yazi::engine;
 
-string get_root_path()
-{
-    char path[1024];
-    memset(path, 0, 1024);
-    int cnt = readlink("/proc/self/exe", path, 1024);
-    if (cnt < 0 || cnt >= 1024)
-    {
-        return "";
-    }
-    for (int i = cnt; i >= 0; --i)
-    {
-        if (path[i] == '/')
-        {
-            path[i] = '\0';
-            break;
-        }
-    }
-    return string(path);
-}
-
 int main()
 {
-    // get the program's root path
-    string root_path = get_root_path();
-
-    // initialize the global logger
-    Logger::instance()->open(root_path + "/main.log");
-
-    // initialize the ini file
-    IniFile * conf = Singleton<IniFile>::instance();
-    conf->load(root_path + "/main.ini");
-    conf->set("root", "path", root_path);
+    System * sys = Singleton<System>::instance();
+    sys->init();
 
     // initialize the work flow
     Workflow * workflow = Singleton<Workflow>::instance();
-    workflow->load(root_path + "/workflow.xml");
+    workflow->load(sys->get_root_path() + "/workflow.xml");
 
     const string & input = "this is input";
 
